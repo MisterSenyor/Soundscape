4c5ae48e5199257924d3edb347a4d5178620d976
Nerya Sheetrit <neryasheetrit@gmail.com>
2021-03-04 11:33:56 +0200

    grogu

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/",function(req,res){
   res.sendFile(__dirname + "/index.html")
 })
 
+app.get("/grogu",function(req,res){
+  res.send("grogu")
+})
+
 app.listen(3000, function(){
   console.log("listening on 3000");
 })
